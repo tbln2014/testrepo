246b2fecc83222ab6d704900d9bc268bc3357104
Tim <Tim@localhost>
2014-12-07 13:38:15 +0100

    Test

--- test/src/test/java/test/TestTest.java
+++ test/src/test/java/test/TestTest.java
@@ -1,8 +1,19 @@
 package test;
 
+import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.runner.notification.Failure;
+
 public class TestTest {
 
+	@Test
 	public void testFailure() {
+		
+	}
+	
+	@Test
+	public void testTest() {
+		System.out.println("Test");
 		
 	}
 }
